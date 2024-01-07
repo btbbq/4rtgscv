3260d64a996a7b12e732b9bf45e5cf664dabbf98
btbbq <145179751+btbbq@users.noreply.github.com>
2024-01-07 11:45:33 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import pyrogram , pyromod
-from config import Config 
+#from config import Config 
 from pyromod import listen
 
 from pyrogram import Client, filters, enums
 from kvsqlite.sync import Client as dt
 p = dict(root='plugins')
-tok = Config.6684742291:AAGq7dMErqDjLOl2841y0hEk7gBDKvTw9uY ## توكنك 
-id = Config.6766128426 ## ايديك
+tok = Config.TG_BOT_TOKEN ## توكنك 
+id = Config.APP_ID ## ايديك
 db = dt("data.sqlite", 'fuck')
 if not db.get("checker"):
   db.set('checker', None)
